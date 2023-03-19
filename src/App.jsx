c3a51a3fc35488a3eb0f34738ff7848a6fb1ebf9
Zowie Erickson <zerickson@apple.com>
2023-03-18 21:46:08 -0400

    use props to generate more card components from data file

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Card from './components/Card'
+import cardData from './data.js'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const cardElements = cardData.map(card => {
+    return <Card
+      key={card.id}
+      img={card.coverImg}
+      rating={card.stats.rating}
+      reviewCount={card.stats.reviewCount}
+      location={card.location}
+      title={card.title}
+      price={card.price}
+      openSpots={card.openSpots}
+    />
+  })
+
 
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Card />
+      <section className="cards-list">
+        {cardElements}
+      </section>
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,28 @@
-import KatieZaferesCard from '../assets/katie-zaferes.png'
 import Star from '../assets/star.png'
 import '../styles/Card.css'
 
-function Card() {
+function Card(props) {
+    let badgeText;
+    if (props.openSpots === 0) {
+        badgeText = 'SOLD OUT'
+    } else if (props.location === 'Online') {
+        badgeText = 'ONLINE'
+    }
+
+
     return (
-        <section className="cards">
             <article className="card">
-                <div className="card__image-container">
-                    <img className="card__image" src={KatieZaferesCard} alt="Katie Zaferes in her swim gear smiling" />
-                    <span className="card__sticker">Sold&nbsp;Out</span>
-                </div>
+                {badgeText && <span className="card__sticker">{badgeText}</span>}
+                <img className="card__image" src={`../../src/assets/${props.img}`} alt="Katie Zaferes in her swim gear smiling" />
                 <div className="card__copy">
                     <div className="card__details">
                         <img className="card__rating-icon" src={Star} alt="" />
-                        <p><span className="card__rating">5.0</span> <span className="gray-text"><span className="card__rating-number">(6)</span> • <span className="card__country">usa</span></span></p>
+                        <p><span className="card__rating">{props.rating}</span> <span className="gray-text"><span className="card__rating-number">({props.reviewCount})</span> • <span className="card__country">{props.location}</span></span></p>
                     </div>
-                    <p className="card__description">Life lessons with Katie Zaferes</p>
-                    <p className="card__price"><span class="strong">From $136</span> / person</p>
+                    <p className="card__description">{props.title}</p>
+                    <p className="card__price"><span className="strong">From ${props.price}</span> / person</p>
                 </div>
             </article>
-        </section>
     )
 }
 
